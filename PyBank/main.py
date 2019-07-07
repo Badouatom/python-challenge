b5e9b6bec18930c7a0c5c56fbbae6cfee6dee4cf
BADOU ARTHUR KOSSONOU <47370403+Badouatom@users.noreply.github.com>
2019-07-07 10:55:29 -0700

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,8 +12,10 @@ max_increase_Amount=0
 max_decrease_Amount=0
 max_decrease_month=0
 max_increase_month=0
+# to read the file
 with open(csvpath,newline='') as csvfile:
    csvreader= csv.reader(csvfile, delimiter=',')
+   #to skip the line header
    next(csvreader)
    # set the date counter to zero value
   # loop through to csv file to find the date number and net total amount
@@ -21,29 +23,31 @@ with open(csvpath,newline='') as csvfile:
       
       Total_Months.append(row[0])
       Total_Revenue.append(int(row[1]))
-      
+      # print Total of Months and Revenue
    print(f"Total Months: {len(Total_Months)}")
    print(f"Total Revenue :${sum(Total_Revenue)}")
-   # loop through to get the Amount
+   # loop through to get the Amount change
    for i in range(len(Total_Revenue)-1):
+      # we add to the monthly amount total, the revenue change between every two months
      Monthly_Amount_Change.append(Total_Revenue[i+1]-Total_Revenue[i])
-         
+        # to dispay the Average change by usinge revenue calculated 
    print(f" The Average Change: {round(sum(Monthly_Amount_Change)/len(Monthly_Amount_Change),2)}")
    
       #look for higher and lower Monthly Amount Change
    max_increase_Amount= max(Monthly_Amount_Change)   
 
    max_decrease_Amount= min(Monthly_Amount_Change)
-   #
+   # to find the max increase and min decrease  months
    max__increase_month=Monthly_Amount_Change.index(max(Monthly_Amount_Change))+1
    max_decrease_month= Monthly_Amount_Change.index(min(Monthly_Amount_Change))+1
+   # those both line below will display the max increase and min decrease revenue and their corresponding date
    print(f" Greatest Increase in Profits:{Total_Months[max_increase_month]} (${(str(max_increase_Amount))})")
    print(f" Greatest Decrease in Profits: {Total_Months[max_decrease_month]} (${(str(max_decrease_Amount))})")
    
        
-   # Output files
+   # this part will fill out our txt file
   
-   
+  # this is to open the writer file 
 with open(output_file,"w") as file:
   # csvwriter = csv.writer(file, delimiter=',')
    # Write methods to print to Financial_Analysis_Summary 
